d3d2b9b42d27edfb02c892a19c256f07d7557f9e
Alexandr Dzyundzya <dzyundzya.aa@yandex.ru>
2024-10-08 14:50:05 +0300

    Recipes models

---
+++ backend/recipes/models.py
@@ -0,0 +1,50 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+from recipes import constants
+
+
+User = get_user_model()
+
+
+class Recipe(models.Model):
+
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipes',
+    )
+    title = models.CharField(
+        'Название рецепта',
+        max_length=constants.MAX_LENGTH_TITLE,
+    )
+    image = models.ImageField(
+        'Картинка рецепта',
+        upload_to='images_recipe/',
+    )
+    text = models.TextField('Описание рецепта')
+    ingredients = models.ManyToManyField(
+        'Ingredient',
+        related_name='recipes',
+    )
+    tags = models.ManyToManyField(
+        'Tag',
+        related_name='recipes',
+    )
+    cooking_time = models.DurationField('Время приготовления')
+
+
+class Ingredient(models.Model):
+    
+    title = models.CharField(
+        'Название инргедиента',
+        max_length=constants.MAX_LENGTH_TITLE,
+    )
+    units_of_measurement = models.CharField(
+        'Единицы измерения',
+        max_length=constants.UNITS_OF_MEASUREMENT,
+    )
+
+
+class Tag(models.Model):
+    pass
