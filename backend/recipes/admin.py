47e223f872707609ab44f493dffd32b0cf56bd4b
Alexandr Dzyundzya <dzyundzya.aa@yandex.ru>
2024-10-09 10:12:03 +0300

    Update Recipes

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Ingredient, Recipe, Tag
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'measurement_unit',
+    )
+    list_filter = (
+        'name',
+    )
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'author',
+    )
+    list_filter = (
+        'name',
+        'author',
+        'tags',
+    )
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'slug',
+    )
+
+
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Tag, TagAdmin)
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -14,9 +14,9 @@ class Recipe(models.Model):
         on_delete=models.CASCADE,
         related_name='recipes',
     )
-    title = models.CharField(
+    name = models.CharField(
         'Название рецепта',
-        max_length=constants.MAX_LENGTH_TITLE_AND_SLUG,
+        max_length=constants.MAX_LENGTH_NAME_AND_SLUG,
     )
     image = models.ImageField(
         'Картинка рецепта',
@@ -33,23 +33,52 @@ class Recipe(models.Model):
     )
     cooking_time = models.DurationField('Время приготовления')
 
+    class Meta:
+
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+
+    def __str__(self):
+        return self.name
+
 
 class Ingredient(models.Model):
-    
-    title = models.CharField(
+
+    name = models.CharField(
         'Название инргедиента',
-        max_length=constants.MAX_LENGTH_TITLE_AND_SLUG,
+        max_length=constants.MAX_LENGTH_NAME_AND_SLUG,
     )
-    units_of_measurement = models.CharField(
+    measurement_unit = models.CharField(
         'Единицы измерения',
         max_length=constants.UNITS_OF_MEASUREMENT,
     )
 
+    class Meta:
+
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+
+    def __str__(self):
+        return self.name
+
 
 class Tag(models.Model):
 
-    title = models.CharField(
+    name = models.CharField(
         'Название тега',
-        max_length=constants.MAX_LENGTH_TITLE_AND_SLUG,
+        max_length=constants.MAX_LENGTH_NAME_AND_SLUG,
+        unique=True,
     )
-    slug = models.SlugField(max_length=constants.MAX_LENGTH_TITLE_AND_SLUG)
+    slug = models.SlugField(
+        'Слаг тега',
+        max_length=constants.MAX_LENGTH_NAME_AND_SLUG,
+        unique=True,
+    )
+
+    class Meta:
+
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.name
