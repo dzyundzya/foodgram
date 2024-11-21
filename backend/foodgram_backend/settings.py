c0a6de3ff931498be4660450ca73434ef5331566
Alexandr Dzyundzya <dzyundzya.aa@yandex.ru>
2024-11-21 21:08:57 +0300

    full refactoring

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='DEFAULT_KEY')
 
-DEBUG = True # os.getenv('DEBUG', 'False') != 'False'
+DEBUG = os.getenv('DEBUG', 'False') != 'False'
 
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1, localhost').split(', ')
 
