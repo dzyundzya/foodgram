33708a5f88389c7d1d9d0a789f94e5e2b90d21bd
Alexandr Dzyundzya <dzyundzya.aa@yandex.ru>
2024-11-26 13:06:22 +0300

    fix create recipe

--- backend/api/v1_api/recipes/views.py
+++ backend/api/v1_api/recipes/views.py
@@ -1,6 +1,5 @@
 from django.db.models import Sum
-from django.http.response import HttpResponse, HttpResponseRedirect
-from django.shortcuts import get_object_or_404
+from django.http.response import HttpResponse
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, status, permissions, viewsets
 from rest_framework.decorators import action
