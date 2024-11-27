535357765950ee5f2b3026e75391662e0f37bea3
Alexandr Dzyundzya <dzyundzya.aa@yandex.ru>
2024-11-27 16:44:21 +0300

    fix create/update recipe

--- backend/recipes/constants.py
+++ backend/recipes/constants.py
@@ -3,6 +3,6 @@ MIN_COOKING_TIME = MIN_AMOUNT = 1
 MAX_LENGTH_INGREDIENT_NAME = 128
 UNITS_OF_MEASUREMENT = 64
 
-HELP_TEXT_NAME = 'Название рецепта, не более 256 символов'
-HELP_TEXT_COOKING_TIME = 'Время приготовления в минутах'
+HELP_TEXT_NAME = 'Название рецепта, не более 256 символов.'
+HELP_TEXT_COOKING_TIME = 'Время приготовления в минутах.'
 HELP_TEXT_UNIT = 'Пример: гр, мл, и т.п.'
