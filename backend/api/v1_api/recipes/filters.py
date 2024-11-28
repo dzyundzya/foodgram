0af8cb46b20387b70c997b020dcbf9e499a2d065
Alexandr Dzyundzya <dzyundzya.aa@yandex.ru>
2024-11-28 13:25:07 +0300

    fix filters recipes

--- backend/api/v1_api/recipes/filters.py
+++ backend/api/v1_api/recipes/filters.py
@@ -7,7 +7,7 @@ from tags.models import Tag
 class RecipeFilter(FilterSet):
 
     tags = filters.ModelMultipleChoiceFilter(
-        field_name='tags_slug',
+        field_name='tags__slug',
         to_field_name='slug',
         queryset=Tag.objects.all(),
     )
