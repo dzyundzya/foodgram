2107fb897db346f2708d26b41c1db389a14707aa
Alexandr Dzyundzya <dzyundzya.aa@yandex.ru>
2024-11-30 21:44:08 +0300

    fix short link

--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -2,10 +2,13 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
+from api.v1_api.recipes.views import short_url
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('s/<int:pk>', short_url, name='short_url')
 ]
 
 if settings.DEBUG:
